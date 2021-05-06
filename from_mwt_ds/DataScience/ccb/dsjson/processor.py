c681860844c9349ac6a0682f45c49b9042186bb1
Alexey Taymanov <ataymano@gmail.com>
2021-05-05 21:07:07 -0400

    actions / slots csv

--- from_mwt_ds/DataScience/ccb/dsjson/processor.py
+++ from_mwt_ds/DataScience/ccb/dsjson/processor.py
@@ -1,6 +1,8 @@
 import ccb.dsjson.filters as filters
 import json
 from collections import ChainMap
+from itertools import chain
+import pandas as pd
 
 # +
 def json_load(line):
@@ -15,10 +17,10 @@ class Processor:
         '_skipLearn': o.get('_skipLearn', False),
         'pdrop': o.get('pdrop', 0.0)}
     
-    default_slot_processor = lambda o: {
+    default_slot_processor = lambda i, o: {
         '_inc': o.get('_inc', [])}
     
-    default_outcome_processor = lambda o: {
+    default_outcome_processor = lambda i, o: {
         '_label_cost': o['_label_cost'],
         '_id': o['_id'],
         '_a': o['_a'],
@@ -29,7 +31,7 @@ class Processor:
         'c': [],
         '_multi': [],
         '_slots': [default_slot_processor],
-        '_outcomes': [default_outcome_processor]
+        '_outcomes': [default_outcome_processor],
     }
 
     filters = [filters.is_decision]
@@ -45,18 +47,38 @@ class Processor:
 
         actions = [None] * len(parsed['c']['_multi'])
         for i, o in enumerate(parsed['c']['_multi']):
-            actions[i] = dict(ChainMap(*[p(o) for p in self.processors['_multi']]))
+            actions[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_multi']]))
 
         slots = [None] * len(parsed['c']['_slots'])
         for i, o in enumerate(parsed['c']['_slots']):
-            slots[i] = dict(ChainMap(*[p(o) for p in self.processors['_slots']]))
+            slots[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_slots']]))
         
         outcomes = [None] * len(parsed['_outcomes'])
         for i, o in enumerate(parsed['_outcomes']):
-            outcomes[i] = dict(ChainMap(*[p(o) for p in self.processors['_outcomes']]))
+            outcomes[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_outcomes']]))
         
         result = dict(ChainMap(top, {'c': dict(ChainMap(shared, {'_multi': actions, '_slots': slots})), '_outcomes': outcomes}))
         return json.dumps(result)
+
+    def _process_decision_csv(self, line):
+        parsed = json_load(line)
+        top = dict(ChainMap(*[p(parsed) for p in self.processors['/']]))
+        shared = dict(ChainMap(*[p(parsed['c']) for p in self.processors['c']]))
+
+        result = []
+        if self.processors['_multi']:
+            for i, o in enumerate(parsed['c']['_multi']):
+                actions = dict(ChainMap(*[p(i, o) for p in self.processors['_multi']]))
+                result.append(dict(ChainMap(top, shared, actions)))
+        else:
+            slots = [None] * len(parsed['c']['_slots'])
+            for i, o in enumerate(parsed['c']['_slots']):
+                slots[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_slots']]))
+            
+            for i, o in enumerate(parsed['_outcomes']):
+                outcomes = dict(ChainMap(*[p(i, o) for p in self.processors['_outcomes']]))
+                result.append(dict(ChainMap(top, shared, slots[i], outcomes)))
+        return result
 
     def process(self, lines):
         for f in self.filters:
@@ -64,3 +86,11 @@ class Processor:
         if len(self.processors) > 1:
             return map(lambda l: f'{self._process_decision(l)}\n', lines) 
         return lines
+
+    def process_csv(self, lines):
+        if self.processors['_multi'] and (self.processors['_slots'] or self.processors['_outcomes']):
+            raise Exception('Cannot process both slots and actions in csv processing')
+        for f in self.filters:
+            lines = filter(lambda l: f(l), lines)
+        return pd.DataFrame(chain.from_iterable(map(lambda l: self._process_decision_csv(l), lines)))
+
