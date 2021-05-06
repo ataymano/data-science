f43b7f217fc3ce1efdcff1dd0991f70870fe9195
Alexey Taymanov <ataymano@gmail.com>
2021-05-06 17:03:43 -0400

    whole object to processor

--- from_mwt_ds/DataScience/ccb/dsjson/processor.py
+++ from_mwt_ds/DataScience/ccb/dsjson/processor.py
@@ -17,14 +17,14 @@ class Processor:
         '_skipLearn': o.get('_skipLearn', False),
         'pdrop': o.get('pdrop', 0.0)}
     
-    default_slot_processor = lambda i, o: {
-        '_inc': o.get('_inc', [])}
+    default_slot_processor = lambda i, s, e: {
+        '_inc': s.get('_inc', [])}
     
-    default_outcome_processor = lambda i, o: {
-        '_label_cost': o['_label_cost'],
-        '_id': o['_id'],
-        '_a': o['_a'],
-        '_p': o['_p']} 
+    default_outcome_processor = lambda i, s, e: {
+        '_label_cost': s['_label_cost'],
+        '_id': s['_id'],
+        '_a': s['_a'],
+        '_p': s['_p']} 
     
     processors = {
         '/': [default_top_processor],
@@ -47,15 +47,15 @@ class Processor:
 
         actions = [None] * len(parsed['c']['_multi'])
         for i, o in enumerate(parsed['c']['_multi']):
-            actions[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_multi']]))
+            actions[i] = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_multi']]))
 
         slots = [None] * len(parsed['c']['_slots'])
         for i, o in enumerate(parsed['c']['_slots']):
-            slots[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_slots']]))
+            slots[i] = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_slots']]))
         
         outcomes = [None] * len(parsed['_outcomes'])
         for i, o in enumerate(parsed['_outcomes']):
-            outcomes[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_outcomes']]))
+            outcomes[i] = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_outcomes']]))
         
         result = dict(ChainMap(top, {'c': dict(ChainMap(shared, {'_multi': actions, '_slots': slots})), '_outcomes': outcomes}))
         return json.dumps(result)
@@ -68,15 +68,15 @@ class Processor:
         result = []
         if self.processors['_multi']:
             for i, o in enumerate(parsed['c']['_multi']):
-                actions = dict(ChainMap(*[p(i, o) for p in self.processors['_multi']]))
+                actions = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_multi']]))
                 result.append(dict(ChainMap(top, shared, actions)))
         else:
             slots = [None] * len(parsed['c']['_slots'])
             for i, o in enumerate(parsed['c']['_slots']):
-                slots[i] = dict(ChainMap(*[p(i, o) for p in self.processors['_slots']]))
+                slots[i] = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_slots']]))
             
             for i, o in enumerate(parsed['_outcomes']):
-                outcomes = dict(ChainMap(*[p(i, o) for p in self.processors['_outcomes']]))
+                outcomes = dict(ChainMap(*[p(i, o, parsed) for p in self.processors['_outcomes']]))
                 result.append(dict(ChainMap(top, shared, slots[i], outcomes)))
         return result
 
