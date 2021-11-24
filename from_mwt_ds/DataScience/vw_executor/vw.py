acb54c059f32c25659d97386ee69ebd84fb8ae8e
Alexey Taymanov <ataymano@gmail.com>
2021-11-24 09:46:03 -0500

    audit support

--- from_mwt_ds/DataScience/vw_executor/vw.py
+++ from_mwt_ds/DataScience/vw_executor/vw.py
@@ -12,70 +12,7 @@ from vw_executor.handlers import _Handlers
 from vw_executor.vw_cache import VwCache
 from vw_executor.handlers import ProgressBars
 from vw_executor.vw_opts import VwOpts, InteractiveGrid
-
-
-def _safe_to_float(num: str, default):
-    try:
-        return float(num)
-    except (ValueError, TypeError):
-        return default
-
-
-def _to(value: str, types: list):
-    for t in types:
-        try:
-            return t(value)
-        except (ValueError, TypeError):
-            ...
-    return value
-
-
-# Helper function to extract example counters and metrics from VW output.
-# Counter lines are preceded by a single line containing the text:
-#   loss     last          counter         weight    label  predict features
-# and followed by a blank line
-# Metric lines have the following form:
-# metric_name = metric_value
-
-def _parse_loss(loss_str):
-    if loss_str.strip()[-1] == 'h':
-        loss_str = loss_str.strip()[:-1]
-    return _safe_to_float(loss_str, None)
-
-
-def _extract_metrics(out_lines):
-    loss_table = {'i': [], 'loss': [], 'since_last': []}
-    metrics = {}
-    try:
-        record = False
-        for line in out_lines:
-            line = line.strip()
-            if record:
-                if line == '':
-                    record = False
-                else:
-                    counter_line = line.split()
-                    try:
-                        count, average_loss, since_last = counter_line[2], counter_line[0], counter_line[1]
-                        average_loss_f = float(average_loss)
-                        since_last_f = float(since_last)
-                        loss_table['i'].append(count)
-                        loss_table['loss'].append(average_loss_f)
-                        loss_table['since_last'].append(since_last_f)
-                    except (ValueError, TypeError):
-                        ...  # todo: handle
-            elif line.startswith('loss'):
-                fields = line.split()
-                if fields[0] == 'loss' and fields[1] == 'last' and fields[2] == 'counter':
-                    record = True
-            elif '=' in line:
-                key_value = [p.strip() for p in line.split('=')]
-                if key_value[0] == 'average loss':
-                    metrics[key_value[0]] = _parse_loss(key_value[1])
-                else:
-                    metrics[key_value[0]] = _to(key_value[1], [int, float])
-    finally:
-        return pd.DataFrame(loss_table).set_index('i'), metrics
+from vw_executor.artifacts import Output, Audit
 
 
 def _save(txt, path):
@@ -93,44 +30,6 @@ class ExecutionStatus(enum.Enum):
     Failed = 4
 
 
-class Output:
-    def __init__(self, path):
-        self.path = path
-        self._processed = False
-        self._loss = None
-        self._loss_table = None
-        self._metrics = None
-
-    def _process(self):
-        self._processed = True
-        self._loss_table, self._metrics = _extract_metrics(self.raw)
-        if 'average loss' in self._metrics:
-            self._loss = self._metrics['average loss']
-
-    @property
-    def raw(self):
-        with open(self.path, 'r') as f:
-            return f.readlines()
-
-    @property
-    def loss(self):
-        if not self._processed:
-            self._process()
-        return self._loss
-
-    @property
-    def loss_table(self):
-        if not self._processed:
-            self._process()
-        return self._loss_table
-
-    @property
-    def metrics(self):
-        if not self._processed:
-            self._process()
-        return self._metrics
-
-
 class Task:
     def __init__(self, job, logger, input_file, input_folder, model_file, model_folder='', no_run=False):
         self._job = job
@@ -145,6 +44,7 @@ class Task:
         self.start_time = None
         self.end_time = None
         self.stdout = None
+        self.audit = None
 
     def _prepare_args(self, cache):
         opts = self._job.opts.copy()
@@ -157,9 +57,10 @@ class Task:
             opts['-i'] = self.model_file
 
         self.outputs_relative = {o: cache.get_path(opts, o, salt) for o in self._job.outputs.keys()}
-        self.outputs = {o: Path(cache.path).joinpath(p) for o, p in self.outputs_relative.items()}
+        self.outputs = {o: cache.path.joinpath(p) for o, p in self.outputs_relative.items()}
 
-        self.stdout_path = Path(cache.path).joinpath(cache.get_path(opts, None, salt, self._logger))
+        self.stdout_path = cache.path.joinpath(cache.get_path(opts, None, salt, self._logger))
+        self.audit_path = cache.path.joinpath(cache.get_path(opts, '-audit', salt, self._logger))
 
         if self.model_file:
             opts['-i'] = Path(self.model_folder).joinpath(self.model_file)
@@ -182,12 +83,14 @@ class Task:
             if self._no_run:
                 raise Exception('Result is not found, and execution is deprecated')
 
-            result = self._execute()
+            audit, result = self._execute()
             _save(result, self.stdout_path)
+            _save(audit, self.audit_path)
         else:
             self._logger.debug(f'Result of vw execution is found: {self.args}')
         self.end_time = time.time()
         self.stdout = Output(self.stdout_path)
+        self.audit = Audit(self.audit_path)
         self.status = ExecutionStatus.Success if self.stdout.loss is not None else ExecutionStatus.Failed
 
     def reset_stdout(self):
@@ -271,6 +174,11 @@ class Job:
     def metrics(self):
         return pd.DataFrame([t.metrics for t in self._tasks])
 
+    @property
+    def audit(self):
+        return pd.concat([t.audit.table.assign(file=i)
+                          for i, t in enumerate(self._tasks)]).reset_index().set_index(['file', 'index'])
+
 
 class TestJob(Job):
     def __init__(self, vw, cache, files, input_dir, opts, outputs, input_mode, no_run, handler, logger):
@@ -308,8 +216,8 @@ class _VwBin:
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
-        error = process.communicate()[1]
-        return error
+        stdout, error = process.communicate()
+        return stdout, error
 
 
 def _run_pyvw(args):
@@ -325,7 +233,7 @@ class _VwPy:
     def run(self, args):
         from multiprocessing import Pool
         with Pool(1) as p:
-            return p.apply(_run_pyvw, [args])
+            return [], p.apply(_run_pyvw, [args])
 
 
 class Vw:
